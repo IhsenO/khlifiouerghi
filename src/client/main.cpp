f61b560cceb642a674206e69e192b67ff5e25d80
IhsenO <ihsen.ouerghi@ensea.fr>
2017-09-21 16:45:20 +0200

    Livrable 1.1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -19,7 +19,7 @@ int main(int argc,char* argv[])
     Exemple exemple;
     exemple.setX(53);
 
-    cout << "It works !" << endl;
+    cout << "Bonjour de la part de Mohamed et Ihsen !" << endl;
 
     return 0;
 }
